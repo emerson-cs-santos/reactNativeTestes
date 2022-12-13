ef91d5c30856417f272774d4381b1f2ec38ce13c
Emerson Costa <emersoncs@outlook.com.br>
2022-12-13 18:28:57 -0300

    fontes e loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading';
 import { Text, View } from 'react-native';
+
+import {
+    useFonts,
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+    Poppins_800ExtraBold
+} from "@expo-google-fonts/poppins";
+
+import { DMSans_400Regular} from "@expo-google-fonts/dm-sans"
+import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
 
 // const App: React.FC = () => {
 //     return (
@@ -10,6 +23,21 @@ import { Text, View } from 'react-native';
 // }
 
 export default function App() {
+
+    const [fontsLoaded] = useFonts ({
+        Poppins_300Light,
+        Poppins_400Regular,
+        Poppins_500Medium,
+        Poppins_700Bold,
+        Poppins_800ExtraBold,
+        DMSans_400Regular,
+        DMSerifDisplay_400Regular
+    })
+
+    if ( !fontsLoaded ) {
+        return <AppLoading/>
+    }
+
     return (
         <View
         style={{
@@ -21,7 +49,8 @@ export default function App() {
             <Text>Wallet/Carteira App</Text>
         </View>
     );
+
   }
 
-export { App };
+//export { App };
 
